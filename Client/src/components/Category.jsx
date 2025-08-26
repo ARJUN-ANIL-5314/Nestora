0e8b4dd350cf9da5cc4c5d7bf2107a0bf95c107d
Arjun Anil <arjunanil5314@gmail.com>
2025-08-26 11:22:14 +0530

    latest update

--- Client/src/components/Category.jsx
+++ Client/src/components/Category.jsx
@@ -25,7 +25,7 @@ export default function CategorySwiper() {
   useEffect(() => {
     const fetchCategories = async () => {
       try {
-        const response = await fetch("http://localhost:3002/category");
+        const response = await fetch("https://nestora-4tme.onrender.com/category");
   
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
--- Client/src/components/Testimonials.jsx
+++ Client/src/components/Testimonials.jsx
@@ -9,7 +9,7 @@ function Testimonials() {
   useEffect(() => {
     const fetchReviews = async () => {
       try {
-        const response = await fetch("http://localhost:3002/reviews");
+        const response = await fetch("https://nestora-4tme.onrender.com/reviews");
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
         }
